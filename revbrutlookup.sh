6508b67b9149feb8f40d6d8fbf0ea02e5a071c73
tushar R <57782379+tusharravindran@users.noreply.github.com>
2022-03-30 19:50:49 +0530

    Update revbrutlookup.sh

--- revbrutlookup.sh
+++ revbrutlookup.sh
@@ -1,1 +1,8 @@
 for ip in $(seq 100 200);do host 104.31.71.$ip;done 
+
+
+#change this value 104.31.71.$ip  to "your ip adress" 
+
+#ifconfig
+#inet 192.168.0.110  netmask 255.255.255.0 
+#you must enter 192.168.0.$ip to bruteforce all available connections in your network
