16f91623244b6382bed4db495d39c8c4ffbba0e3
tushar R <57782379+tusharravindran@users.noreply.github.com>
2022-03-30 19:41:22 +0530

    Create array_implementation.sh

---
+++ array_implementation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Declare array with 4 elements
+ARRAY=( 'Debian Linux' 'Redhat Linux' Ubuntu Linux )
+
+
+
+# get number of elements in the array
+ELEMENTS=${#ARRAY[@]}
+
+
+# for loop
+for (( i=0;i<$ELEMENTS;i++)); do
+    echo ${ARRAY[${i}]}
+done 
